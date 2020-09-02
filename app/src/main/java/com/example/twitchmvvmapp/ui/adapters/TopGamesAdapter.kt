b17e7c76cca3bc39f00581af4e44cead9019fb2b
LyfFhakx <warsongnowow@gmail.com>
2020-09-02 19:42:52 +0700

    adapter completed

--- app/src/main/java/com/example/twitchmvvmapp/ui/adapters/TopGamesAdapter.kt
+++ app/src/main/java/com/example/twitchmvvmapp/ui/adapters/TopGamesAdapter.kt
@@ -1,18 +1,32 @@
 package com.example.twitchmvvmapp.ui.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.twitchmvvmapp.R
 import com.example.twitchmvvmapp.data.models.Top
+import com.example.twitchmvvmapp.data.models.TopGames
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_top_preview.view.*
 
 class TopGamesAdapter : RecyclerView.Adapter<TopGamesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopGamesViewHolder {
-        TODO("Not yet implemented")
+       return TopGamesViewHolder(
+           LayoutInflater.from(parent.context)
+               .inflate(R.layout.item_top_preview,parent,false)
+       )
     }
 
     override fun onBindViewHolder(holder: TopGamesViewHolder, position: Int) {
-        val top = differ.currentList[position]
+        val topGames = differ.currentList[position]
+        holder.itemView.apply{
+            Picasso.get().load(topGames.game.logo.medium).into(ivLogo)
+            tvTitle.text = topGames.game.name
+            tvChannels.text = topGames.top.channels.toString()
+            tvViewers.text = topGames.top.viewers.toString()
+        }
     }
 
     override fun getItemCount(): Int {
@@ -20,12 +34,12 @@ class TopGamesAdapter : RecyclerView.Adapter<TopGamesViewHolder>() {
     }
 
 
-    private val differCallback = object : DiffUtil.ItemCallback<Top>() {
-        override fun areContentsTheSame(oldItem: Top, newItem: Top): Boolean {
-            return oldItem.id == newItem.id
+    private val differCallback = object : DiffUtil.ItemCallback<TopGames>() {
+        override fun areContentsTheSame(oldItem: TopGames, newItem: TopGames): Boolean {
+            return (oldItem.top.id == newItem.top.id && oldItem.game._id == newItem.game._id)
         }
 
-        override fun areItemsTheSame(oldItem: Top, newItem: Top): Boolean {
+        override fun areItemsTheSame(oldItem: TopGames, newItem: TopGames): Boolean {
             return oldItem == newItem
         }
     }
